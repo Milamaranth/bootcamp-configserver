feb6cbe504d64676b4c7f450c8e6646ada2efa57
milamaranth <mila.sanchezcarrero@gmail.com>
2022-10-03 12:51:21 +0200

    config

---
+++ src/test/java/com/nttdata/bootcamp/productserviceconfig/ProductserviceconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.productserviceconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductserviceconfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
